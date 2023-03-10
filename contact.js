bf91e6cd0850a4df832f618525e8857b76ebe44d
emon3455 <emon.mhk69@gmail.com>
2023-03-11 02:29:23 +0600

    contact me project done

--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
 document.getElementById("send-name").addEventListener("click", ()=>{
     const value = document.getElementById("name-input").value;
     localStorage.setItem("name",value);
+    document.getElementById("name-input").value="";
 });
 
 document.getElementById("delete-name").addEventListener("click" , ()=>{
@@ -10,6 +11,7 @@ document.getElementById("delete-name").addEventListener("click" , ()=>{
 document.getElementById("send-mail").addEventListener("click", ()=>{
     const value = document.getElementById("mail-input").value;
     localStorage.setItem("mail",value);
+    document.getElementById("mail-input").value="";
 });
 
 document.getElementById("delete-mail").addEventListener("click" , ()=>{
@@ -20,11 +22,40 @@ document.getElementById("delete-mail").addEventListener("click" , ()=>{
 document.getElementById("send-message").addEventListener("click", ()=>{
     const value = document.getElementById("message-input").value;
     localStorage.setItem("message",value);
+    document.getElementById("message-input").value="";
 });
 
 document.getElementById("delete-message").addEventListener("click" , ()=>{
     localStorage.removeItem("message");
 });
+// -------------nasic add and delete done------------
+
+// function for clear all.
+document.getElementById("reset").addEventListener("click" , ()=>{
+    localStorage.clear();
+    document.getElementById("name-input").value="";
+    document.getElementById("mail-input").value="";
+    document.getElementById("message-input").value="";
+})
+
+// function to set all:
+document.getElementById("send-all").addEventListener("click", ()=>{
+
+    let obj ={};
+    const name = document.getElementById("name-input").value;
+    const mail = document.getElementById("mail-input").value;
+    const message = document.getElementById("message-input").value;
+
+    obj.name=name;
+    obj.mail=mail;
+    obj.message=message;
+
+    const stringOBJ = JSON.stringify(obj);
+
+    localStorage.setItem("user",stringOBJ);
+
+
+})
 
 
 
